6199caf5cbee6fc21fc60019c965fa48c144112a
Hiroaki Honda <d@damin.dev>
2025-04-15 21:54:05 +0900

    Add initial setup and metadata for HGGD package

---
+++ hggd/__init__.py
@@ -0,0 +1,5 @@
+"""
+HGGD: Efficient Heatmap-Guided 6-Dof Grasp Detection in Cluttered Scenes
+"""
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hggd",
+    version="0.1.0",
+    author="THU-VCLab",
+    description="Efficient Heatmap-Guided 6-Dof Grasp Detection in Cluttered Scenes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/THU-VCLab/HGGD",
+    packages=find_packages(include=["hggd", "hggd.*", "customgraspnetAPI", "customgraspnetAPI.*", 
+                                   "dataset", "dataset.*", "models", "models.*"]),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy==1.23.5",
+        "pandas",
+        "numba",
+        "matplotlib",
+        "open3d",
+        "opencv-python",
+        "scikit-image",
+        "tensorboardX",
+        "torchsummary",
+        "tqdm",
+        "transforms3d",
+        "trimesh",
+        "autolab_core",
+        "cvxopt",
+    ],
+    # These dependencies require special installation
+    extras_require={
+        "full": [
+            "torch>=1.10.0",
+            "pytorch3d",
+            "cupoch",
+            "grasp_nms",
+        ]
+    },
+)
